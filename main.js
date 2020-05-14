024bf30bfd6bc7c580dcdfacd25d3cbab0239513
novin997 <cocolupiayi@gmail.com>
2020-05-14 16:36:27 +0800

    change orientation of musicPlayer

--- main.js
+++ main.js
@@ -1,6 +1,10 @@
 const widgetIframe = document.getElementById("soundCloud");
 const widget = SC.Widget(widgetIframe);
 
+const playButton = document.getElementById("playPauseButton");
+const nextButton = document.getElementById("nextButton");
+const prevButton = document.getElementById("prevButton");
+
 let slider = document.getElementById("volumeRange")
 
 //Set default volume for soundCloud
@@ -20,4 +24,16 @@ widget.bind(SC.Widget.Events.READY, function()
 widget.bind(SC.Widget.Events.PLAY_PROGRESS, function() 
 {
 	widget.setVolume(volume);
+});
+
+widget.bind(SC.Widget.Events.READY, function() {
+	playButton.addEventListener("click",() => widget.toggle());
+});
+
+widget.bind(SC.Widget.Events.READY, function() {
+	nextButton.addEventListener("click",() => widget.next());
+});
+
+widget.bind(SC.Widget.Events.READY, function() {
+	prevButton.addEventListener("click",() => widget.prev());
 });
