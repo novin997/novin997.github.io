dfc5ee5e0716e2d1d9f313a0764cf51eebdbea51
novin997 <cocolupiayi@gmail.com>
2020-06-01 20:12:22 +0800

    add filtering based on anime title

--- anime/anime.js
+++ anime/anime.js
@@ -1,8 +1,18 @@
 const searchBar = document.querySelector("#searchBar");
 
-searchBar.addEventListener('input',() => {
-    console.log("Hello");
+searchBar.addEventListener('input',(e) => {
+    let searchString = e.target.value.toLowerCase();
+    const cards = document.querySelectorAll(".card-header");
+    
+    cards.forEach((item)=>{
+        console.log(item);
+        if(item.innerHTML.toLowerCase().indexOf(searchString)!= -1)
+        {
+            item.parentNode.parentNode.style.display = "";
+        }
+        else
+        {
+            item.parentNode.parentNode.style.display = "none";
+        }
+    });
 });
-
-// console.log("Hello");
-
