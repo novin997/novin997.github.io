b20a176b26e9c6e04321cc58da40599167c1f9d2
novin997 <cocolupiayi@gmail.com>
2020-06-03 16:05:44 +0800

    update search functions including tags search

--- anime/anime.js
+++ anime/anime.js
@@ -2,17 +2,26 @@ const searchBar = document.querySelector("#searchBar");
 
 searchBar.addEventListener('input',(e) => {
     let searchString = e.target.value.toLowerCase();
-    const cards = document.querySelectorAll(".card-header");
+    const cards = document.querySelectorAll(".card");
     
+    //For each card
     cards.forEach((item)=>{
-        console.log(item);
-        if(item.innerHTML.toLowerCase().indexOf(searchString)!= -1)
+        //Get the card header selector
+        const cardHeader = item.childNodes[1];
+        const cardTag = item.querySelectorAll(".badge");
+        if(cardHeader.innerHTML.toLowerCase().indexOf(searchString)!= -1)
         {
-            item.parentNode.parentNode.style.display = "";
+            item.parentNode.style.display = "";
         }
         else
         {
-            item.parentNode.parentNode.style.display = "none";
+            item.parentNode.style.display = "none";
         }
+        
+        //Loop through each tag in the GPU
+        cardTag.forEach((tag) => {
+            if(tag.innerHTML.toLowerCase().indexOf(searchString)!= -1 && item.parentNode.style.display == "none")
+                item.parentNode.style.display = "";             
+        })
     });
 });
