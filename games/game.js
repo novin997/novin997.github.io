1dc6158cc6a71edb9ce00e05c6519c5013bcf2fe
novin997 <cocolupiayi@gmail.com>
2020-06-06 19:25:54 +0800

    update game with hover css

---
+++ games/game.js
@@ -0,0 +1,16 @@
+const TicTatToe = document.querySelector(".gameboard");
+const box = document.querySelectorAll(".boardOutline");
+
+function setupGame(){
+    let board = [
+        [null,null,null],
+        [null,null,null],
+        [null,null,null]
+    ];
+}
+
+box.addEventListener("click",(e)=>{
+    console.log("test");   
+});
+
+
