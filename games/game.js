7d841814a3464512954ea6bed0884ef4bd093665
novin997 <cocolupiayi@gmail.com>
2020-06-09 17:16:49 +0800

    update tic tat toe with cross and circle

--- games/game.js
+++ games/game.js
@@ -5,21 +5,61 @@ let board = [
     [null,null,null],
     [null,null,null]
 ];
+let curr = "circle";
 
 function setupGame(){
     boxs.forEach((box,i)=>{
         // Reset all the initial board value to null and set the initial board for circle first
         board[i] = null;
-        console.log(board[i]); 
         box.classList.remove("circle","cross","empty-cross");
         box.classList.add("empty-circle");
-        console.log(box.classList);
     });
+    curr = "circle";
 };
 
-boxs.forEach((box)=> {
+function changeTurn()
+{
+    if(curr=="circle")
+    {
+        curr = "cross";
+        boxs.forEach((box,index)=> {
+            if(board[index]==null)
+            {
+                box.classList.remove("empty-circle");
+                box.classList.add("empty-cross");
+                console.log(box.classList);      
+            }
+        }); 
+    }    
+    else
+    {
+        curr = "circle";
+        boxs.forEach((box,index)=> {
+            if(board[index]==null)
+            {
+                box.classList.remove("empty-cross");
+                box.classList.add("empty-circle");
+                console.log(box.classList);      
+            }
+        });
+    }           
+};
+
+boxs.forEach((box,index)=> {
     box.addEventListener("click",(e)=>{
-        console.log("test");   
+        if(curr=="circle" && board[index]==null)
+        {
+            board[index]="circle";
+            box.classList.remove("empty-circle");
+            box.classList.add("circle");
+        }
+        else if(curr=="cross" && board[index]==null)
+        {
+            board[index]="cross";
+            box.classList.remove("empty-cross");
+            box.classList.add("cross");
+        }
+        changeTurn();        
     });
 });
 
