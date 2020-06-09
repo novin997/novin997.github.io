a238f11ee3b0051689fcedfc29b6714a709ab2d1
novin997 <cocolupiayi@gmail.com>
2020-06-09 21:36:48 +0800

    add win conditions to tic tat toe

--- games/game.js
+++ games/game.js
@@ -5,6 +5,17 @@ let board = [
     [null,null,null],
     [null,null,null]
 ];
+
+let winConditions = [
+    [0,1,2],
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,4,6],
+    [2,5,8],
+    [3,4,5],
+    [6,7,8],
+];
 let curr = "circle";
 
 function setupGame(){
@@ -26,8 +37,7 @@ function changeTurn()
             if(board[index]==null)
             {
                 box.classList.remove("empty-circle");
-                box.classList.add("empty-cross");
-                console.log(box.classList);      
+                box.classList.add("empty-cross");    
             }
         }); 
     }    
@@ -38,11 +48,22 @@ function changeTurn()
             if(board[index]==null)
             {
                 box.classList.remove("empty-cross");
-                box.classList.add("empty-circle");
-                console.log(box.classList);      
+                box.classList.add("empty-circle");     
             }
         });
     }           
+};
+
+function checkWinCond()
+{
+    // check if anyone has won the tic tat toe
+    winConditions.forEach((val,i)=>{
+        if(board[val[0]]==board[val[1]] && board[val[1]]==board[val[2]] && board[val[0]]!=null) 
+        {
+            console.log("game has ended");
+            setupGame();
+        }
+    });
 };
 
 boxs.forEach((box,index)=> {
@@ -59,7 +80,8 @@ boxs.forEach((box,index)=> {
             box.classList.remove("empty-cross");
             box.classList.add("cross");
         }
-        changeTurn();        
+        checkWinCond();
+        changeTurn();               
     });
 });
 
