e794372c1a79ea25547a79ff041bb56cc90f33d6
novin997 <cocolupiayi@gmail.com>
2020-06-28 18:20:35 +0800

    fix next button for slideshow

--- index.js
+++ index.js
@@ -3,12 +3,21 @@ const prevButton = document.querySelector(".prev");
 const slideShow = document.querySelectorAll(".slideshow-text");
 var slideIndex = 0;
 
-console.log(slideShow.length);
-
 slideShow.forEach((text,index)=>{
     text.addEventListener("animationend",()=>{
-        text.classList.remove("slide-out");
-        text.classList.remove("show");
+        if(text.classList.contains("slide-out"))
+        {
+            text.classList.remove("slide-out");
+            text.classList.remove("show");
+            if(index + 1 == slideShow.length)
+                index = -1;
+            slideShow[index+1].classList.add("show");    
+            slideShow[index+1].classList.add("slide-in");
+        }
+        else if(text.classList.contains("slide-in"))
+        {
+            text.classList.remove("slide-in");
+        }     
     });
     
     if(index == slideIndex)
@@ -18,7 +27,14 @@ slideShow.forEach((text,index)=>{
 });
 
 nextButton.addEventListener("click",() => {
+    
     slideShow[slideIndex].classList.add("slide-out");
+    slideIndex++;
+    if(slideIndex == slideShow.length)
+        slideIndex = 0;
+
+    console.log(slideIndex);
+
     // slideShow[slideIndex].classList.remove("show");
     // slideIndex++;
     // if(slideShow.length == slideIndex)
