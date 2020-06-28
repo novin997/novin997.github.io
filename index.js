ce4b0a237e2d2b1c49ab3226679e66877b91cf4c
novin997 <cocolupiayi@gmail.com>
2020-06-28 19:19:32 +0800

    fix prev and next buttons

--- index.js
+++ index.js
@@ -3,27 +3,33 @@ const prevButton = document.querySelector(".prev");
 const slideShow = document.querySelectorAll(".slideshow-text");
 var slideIndex = 0;
 
+slideShow[slideIndex].classList.add("show");
+
 slideShow.forEach((text,index)=>{
     text.addEventListener("animationend",()=>{
         if(text.classList.contains("slide-out"))
         {
             text.classList.remove("slide-out");
             text.classList.remove("show");
-            if(index + 1 == slideShow.length)
-                index = -1;
-            slideShow[index+1].classList.add("show");    
-            slideShow[index+1].classList.add("slide-in");
+            slideShow[slideIndex].classList.add("show");    
+            slideShow[slideIndex].classList.add("slide-in");
         }
         else if(text.classList.contains("slide-in"))
         {
             text.classList.remove("slide-in");
-        }     
+        }
+        else if(text.classList.contains("slide-out-right"))
+        {
+            text.classList.remove("slide-out-right");
+            text.classList.remove("show");
+            slideShow[slideIndex].classList.add("show");    
+            slideShow[slideIndex].classList.add("slide-in-left");  
+        }
+        else if(text.classList.contains("slide-in-left"))
+        {
+            text.classList.remove("slide-in-left");
+        }    
     });
-    
-    if(index == slideIndex)
-    {
-        text.classList.add("show");
-    }
 });
 
 nextButton.addEventListener("click",() => {
@@ -31,25 +37,12 @@ nextButton.addEventListener("click",() => {
     slideShow[slideIndex].classList.add("slide-out");
     slideIndex++;
     if(slideIndex == slideShow.length)
-        slideIndex = 0;
-
-    console.log(slideIndex);
-
-    // slideShow[slideIndex].classList.remove("show");
-    // slideIndex++;
-    // if(slideShow.length == slideIndex)
-    // {
-    //     slideIndex = 0;
-    // }
-    // slideShow[slideIndex].classList.add("show");        
+        slideIndex = 0;     
 });
 
 prevButton.addEventListener("click",() => {
-    slideShow[slideIndex].classList.remove("show");
+    slideShow[slideIndex].classList.add("slide-out-right");
     slideIndex--;
     if(slideIndex == -1)
-    {
         slideIndex = slideShow.length-1;
-    }
-    slideShow[slideIndex].classList.add("show");        
 });
